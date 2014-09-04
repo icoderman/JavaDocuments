c17534008aae980aed8603b22fa7205366030518
iCoderman <icoderman@gmail.com>
2014-09-04 15:54:59 +0300

    search Rest service

--- src/main/java/org/javadocuments/controller/DocumentController.java
+++ src/main/java/org/javadocuments/controller/DocumentController.java
@@ -1,8 +1,13 @@
 package org.javadocuments.controller;
 
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.SolrInputDocument;
+import org.codehaus.jackson.map.util.JSONPObject;
 import org.javadocuments.dao.DocumentDAO;
 import org.javadocuments.dao.DocumentDAOImpl;
 import org.javadocuments.domain.Document;
@@ -13,7 +18,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping(value="/document")
@@ -21,7 +26,10 @@ public class DocumentController {
 
     @Autowired
     private DocumentService documentService;
-
+/*
+    @Autowired
+    private HttpSolrServer solrServer;
+*/
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Document getDocument(@PathVariable Integer id) {
        return documentService.getDocumentById(id);
@@ -38,10 +46,23 @@ public class DocumentController {
         return documentService.getDocumentById(id);
     }
 
+    @RequestMapping(value="search", method = RequestMethod.POST)
+    public SolrDocumentList serachDocuments(@RequestParam HashMap<String, String> searchTerms) throws SolrServerException {
+        HttpSolrServer solrServer = new HttpSolrServer("http://localhost:8080/solr/javadocuments");
+        System.out.println(searchTerms);
+        SolrQuery query = new SolrQuery();
+        for (Object objKey : searchTerms.keySet()) {
+            query.setQuery(objKey.toString()+":"+searchTerms.get(objKey));
+        }
+        QueryResponse response = solrServer.query(query);
+        SolrDocumentList documentList = response.getResults();
+        return documentList;
+    }
+
     @RequestMapping(value = "/indexAll", method = RequestMethod.GET)
-    public boolean indexAllDocuments() {
+    public boolean indexAllDocuments()  {
         List<Document> docList = documentService.getAllDocuments();
-        HttpSolrServer server = new HttpSolrServer("http://localhost:8080/solr/javadocuments");
+        HttpSolrServer solrServer = new HttpSolrServer("http://localhost:8080/solr/javadocuments");
 
         for(Document document: docList) {
             SolrInputDocument doc = new SolrInputDocument();
@@ -54,7 +75,7 @@ public class DocumentController {
             doc.addField("createddate", document.getCreatedDate());
 
             try {
-                server.add(doc);
+                solrServer.add(doc);
             } catch (SolrServerException e) {
                 e.printStackTrace();
             } catch (IOException e) {
@@ -63,7 +84,7 @@ public class DocumentController {
         }
 
         try {
-            server.commit();
+            solrServer.commit();
         } catch (SolrServerException e) {
             e.printStackTrace();
         } catch (IOException e) {
