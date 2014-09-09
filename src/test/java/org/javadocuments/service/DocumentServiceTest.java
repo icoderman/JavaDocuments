8c97574c36823110c23dae6b3d5e80dddacae065
iCoderman <icoderman@gmail.com>
2014-09-09 17:59:53 +0300

    unit test

---
+++ src/test/java/org/javadocuments/service/DocumentServiceTest.java
@@ -0,0 +1,16 @@
+package org.javadocuments.service;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:test-context.xml")
+public class DocumentServiceTest {
+
+    @Autowired
+    private DocumentService documentService;
+
+}
